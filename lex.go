888feab2b236eef8c0d8bfe5648885cb49383566
Daisuke Maki <JP11194@w022311308173m.local>
2014-03-28 15:48:17 +0900

    initial commit

---
+++ lex.go
@@ -0,0 +1,69 @@
+package atnodes
+
+import (
+  "github.com/lestrrat/go-lex"
+)
+
+const (
+  ItemText  lex.LexItemType = lex.ItemDefaultMax + 1 + iota
+  ItemRange
+  ItemOpenSquareBracket
+  ItemCloseSquareBracket
+)
+
+type Lexer struct {
+  *lex.StringLexer
+}
+
+func NewLexer(input string) *Lexer {
+  l := &Lexer {
+    lex.NewStringLexer(input),
+  }
+  l.SetLexFn("__START__", lexText)
+  return l
+}
+
+func lexText(l lex.Lexer, ctx interface {}) lex.LexFn {
+  al := ctx.(*Lexer)
+
+  for {
+    if al.Peek() == '[' {
+      if al.Cursor() > al.LastCursor() {
+        al.Emit(ItemText)
+      }
+      return lexRange
+    }
+    if al.Next() == lex.EOF {
+      break
+    }
+  }
+
+  if al.Cursor() > al.LastCursor() {
+    al.Emit(ItemText)
+  }
+
+  l.Emit(lex.ItemEOF)
+  return nil
+}
+
+func lexRange(l lex.Lexer, ctx interface {}) lex.LexFn {
+  al := ctx.(*Lexer)
+  if c := al.Next(); c != '[' {
+    return al.EmitErrorf("Expected '[', got %c", c)
+  }
+  al.Emit(ItemOpenSquareBracket)
+
+  for {
+    c := al.Next()
+    switch c {
+    case lex.EOF:
+      return al.EmitErrorf("Expected range, got EOF")
+    case ']':
+      al.Backup()
+      l.Emit(ItemRange)
+      al.Next()
+      l.Emit(ItemCloseSquareBracket)
+      return lexText
+    }
+  }
+}
---
+++ lex_test.go
@@ -0,0 +1,14 @@
+package atnodes
+
+import (
+  "testing"
+)
+
+func TestLex(t *testing.T) {
+  l := NewLexer("www[101-110].example.com")
+  go l.Run(l)
+
+  for item := range l.Items() {
+    t.Logf("%#v", item)
+  }
+}
